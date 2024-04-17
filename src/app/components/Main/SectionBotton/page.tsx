c768b6021dd5c767f51fdc7fbb30ebc257eee117
jsha64 <jorgemerino743@gmail.com>
2024-04-17 01:48:15 -0500

    Organizando carpetas

---
+++ src/app/components/Main/SectionBotton/page.tsx
@@ -0,0 +1,135 @@
+import process from "process";
+export const SectionBoton = () => {
+    return (
+        <>
+        <h1></h1>
+        <div>
+            <p></p>
+        <div>
+            <h1></h1>
+            <p></p>
+            <p></p>
+            <p></p>
+            <p></p>
+            <h1></h1>
+        </div>
+        <div>
+            <h1></h1>
+            <p></p>
+            <p></p>
+            <p></p>
+            <p></p>
+            <h1></h1>
+        </div>
+        <div>
+            <h1></h1>
+            <p></p>
+            <p></p>
+            <p></p>
+            <p></p>
+            <h1></h1>
+        </div>
+        <div>
+            <h1></h1>
+            <p></p>
+            <p></p>
+            <p></p>
+            <p></p>
+            <h1></h1>
+        </div>
+        </div>
+
+        <div>
+            <h3><p></p></h3>
+            <h3><p></p></h3>
+            <h3><p></p></h3>
+            <h1></h1>
+            <p></p>
+        </div>
+
+        <div>
+            <h1></h1>
+            <h2></h2>
+            <p></p>
+            <p></p>
+            <p></p>
+            <p></p>
+            <p></p>
+            <p></p>
+            <p></p>
+            <p></p>
+            <p></p>
+            <p></p>
+            <p></p>
+            <p></p>
+            <p></p>
+            <p></p>
+            <p></p>
+            <p></p>
+        </div>
+
+        <div>
+            <h1></h1>
+            <h1></h1>
+            <h3></h3>
+            <img src="" alt="" />
+            <h3></h3>
+            <img src="" alt="" />
+            <h3></h3>
+            <img src="" alt="" />
+        </div>
+
+        <div>
+            <h1></h1>
+            <audio src=""></audio>
+        </div>
+
+        <div>
+            <h1></h1>
+            <video src=""></video>
+        </div>
+
+        <div>
+            <h1></h1>
+        </div>
+
+        <div>
+            <h1></h1>
+        </div>
+
+        <div>
+            <h1></h1>
+            <meter></meter>
+        </div>
+
+        <div>
+            <h1></h1>
+            <div></div>
+        </div>
+
+        <div>
+            <h1></h1>
+            <div></div>
+        </div>
+
+        <div>
+            <h1></h1>
+            <input type="text" />
+        </div>
+
+        <div>
+            <form action="">
+                <input type="text" />
+                <input type="text" />
+                <input type="text" />
+                <input type="text" />
+                <input type="text" />
+                <input type="text" />
+                <input type="text" />
+                <input type="text" />
+                <input type="text" />
+            </form>
+        </div>
+        </>
+    );  
+}
---
+++ src/app/components/Main/SectionCenter/page.tsx
@@ -0,0 +1,34 @@
+export const SectionPageMain = () => {
+    return (
+        <>
+            <h1></h1>
+            <h1></h1>
+            <p>
+                <p></p>
+            </p>
+
+            <h1></h1>
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+
+            <h1></h1>
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+
+            <h1></h1>
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+
+            <h1></h1>
+        </>
+    );
+}
