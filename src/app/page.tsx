f003570da67155a85fab7862d96b850ee8282204
jsha64 <jorgemerino743@gmail.com>
2024-04-22 21:39:23 -0500

    Agregando Text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex flex-col items-center justify-between p-24">
+      <div className="font-bold text-xl">
+        <h1 id="text">Esto es un texto de ejemplo</h1>
+      </div>  
+    </main>
   );
 }
